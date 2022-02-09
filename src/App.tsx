fff70e658eb13cb15be132b9bdb57a763f53049a
Lucas Segurado <segurado1996@gmail.com>
2022-02-09 18:48:20 -0300

    Added linters

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ function App() {
 
   useEffect(() => {
     dispatch(getReviewsThunk(filters))
-  }, [filters])
+  }, [filters, dispatch])
 
   return (
     <>
--- src/components/PublishedAt.tsx
+++ src/components/PublishedAt.tsx
@@ -3,7 +3,7 @@ import { FC } from "react";
 export const PublishedAt: FC<{ date: string }> = ({ date }) => {
     const parsedDate = new Date(date);
 
-    const [month, _literal, day, _literal2, year] = Intl.DateTimeFormat('en', { day: 'numeric', month: 'long', year: 'numeric' }).formatToParts(parsedDate);
+    const [month, , day, , year] = Intl.DateTimeFormat('en', { day: 'numeric', month: 'long', year: 'numeric' }).formatToParts(parsedDate);
 
     return <span>
         Reviewed {day.value} {month.value} {year.value}
